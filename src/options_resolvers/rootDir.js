08830c9fda8d4db986b40462c52e6170e88d28f5
Kari Kammonen <kari.kammonen@gmail.com>
2017-09-15 22:44:56 +0300

    path handling

--- src/options_resolvers/rootDir.js
+++ src/options_resolvers/rootDir.js
@@ -1,7 +1,7 @@
 import { isAbsolute } from 'path';
 import { statSync } from 'fs';
 import { isString } from '../utils';
-
+import path from 'path';
 /**
  * Resolves rootDir option for css-modules-require-hook
  *
@@ -12,9 +12,17 @@ export default function rootDir(value/* , currentConfig */) {
     if (!isString(value)) {
         throw new Error(`Configuration 'rootDir' is not a string`);
     }
-
-    if (!isAbsolute(value) || !statSync(value).isDirectory()) {
-        throw new Error(`Configuration 'rootDir' is not containg a valid absolute path`);
+    console.log("HERE WE ARE .............")
+    if (!isAbsolute(value) ) {
+        if(!statSync(value).isDirectory()) {
+          throw new Error(`Configuration 'rootDir' is not containg a valid absolute path`);
+        }
+    } else {
+      const workDir = process.cwd()
+      const returnable = path.join(workDir, value)
+      console.log("path was")
+      console.log(returnable)
+        return returnable
     }
 
     return value;
