4e85018c9f17cda80f309d35a997198c0763ec47
Kari Kammonen <kari.kammonen@gmail.com>
2017-09-15 22:59:34 +0300

    included build

---
+++ build/options_resolvers/rootDir.js
@@ -0,0 +1,42 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = rootDir;
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _fs = require('fs');
+
+var _utils = require('../utils');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Resolves rootDir option for css-modules-require-hook
+ *
+ * @param {*} value
+ * @returns {String}
+ */
+function rootDir(value /* , currentConfig */) {
+    if (!(0, _utils.isString)(value)) {
+        throw new Error('Configuration \'rootDir\' is not a string');
+    }
+    console.log("HERE WE ARE .............");
+    if (!(0, _path.isAbsolute)(value)) {
+        if (!(0, _fs.statSync)(value).isDirectory()) {
+            throw new Error('Configuration \'rootDir\' is not containg a valid absolute path');
+        }
+    } else {
+        var workDir = process.cwd();
+        var returnable = _path2.default.join(workDir, value);
+        console.log("path was");
+        console.log(returnable);
+        return returnable;
+    }
+
+    return value;
+}
